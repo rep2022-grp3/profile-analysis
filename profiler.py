58a7a352c5ec1adf630e0a8dbd47e22c92c00fdc
Tobias Haposan <tobias.haposan@student.pmsbe.ac.id>
2022-04-14 08:09:56 +0700

    Commit 14042022

---
+++ profiler.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import csv
+
+#load files
+dfMaster = pd.read_csv(r'C:\Users\tobia\Videos\REP3\profile-analysis\csv-er-RUN1.csv')
+print(df)
+#load weather profiler
+dfWeatherProfiler = pd.read_csv(r'C:\Users\tobia\Videos\REP3\profile-analysis\csv-WeatherInput-RUN1.csv')
+
+#daily input
+occupancyRate = 0.5 #fill this with a range between 0 to 1
+
+#load profile index from array
+
+
+
+
+
+
+
+#write new csv
+
+
+
